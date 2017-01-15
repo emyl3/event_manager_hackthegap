e473c5e88db70a9e4f1844783ca218308c1c5f3f
elisa lee <elisa.my.lee@gmail.com>
2017-01-15 09:18:49 -0600

    more stuff

--- public/scripts/events.controller.js
+++ public/scripts/events.controller.js
@@ -4,20 +4,32 @@ function EventsController(EventsService) {
   ctrl = this;
   ctrl.eventsSelect = true;
 
-
   function getEvents() {
     EventsService.getAllEvents().then(function(res){
-      console.log(res);
       ctrl.eventObject = res;
+      return ctrl.eventObject;
     });
   }
+
+  function showContact () {
+    EventsService.getAttendees().then(function (res) {
+      ctrl.contacts = res;
+    });
+  };
 
   ctrl.openEvent = function(data) {
     ctrl.eventsSelect = false;
     EventsService.getEvent(data).then(function(res) {
       ctrl.eventInfo = res;
+      return res;
+    }).then(function(res) {
+      EventsService.getItems(data).then(function(res) {
+        console.log(res);
+        ctrl.itemsList = res;
+      });
     });
   };
 
   getEvents();
+  showContact();
 }
